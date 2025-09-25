69245b0768c46ac4c9a1dd1af77879f73fed24d4
selvam54 <s.selvam00000001@gmail.com>
2025-09-25 21:26:12 +0530

    gn game

---
+++ guessnumber.py
@@ -0,0 +1,20 @@
+import random
+
+print("********************************guess number game*********************************************")
+
+#player choice
+while True:
+    playerchoice=input(("\n1\n2\n3\n4\n5 \nenter the any number "))
+
+#computer choice
+    list=[ "1","2","3","4","5"]
+    computerchoice=random.choice(list)
+    print("the computer choice is: ",computerchoice)
+
+
+#comperission the two choices
+    if playerchoice == computerchoice:
+      print("you correctly guess the number")
+    else:
+      print("you are lose")
+
