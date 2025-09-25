914c25c638c96d79bb72283bb44a3c2f469ef53e
selvam54 <s.selvam00000001@gmail.com>
2025-09-25 20:31:06 +0530

    rps project

---
+++ rpsgame.py
@@ -0,0 +1,23 @@
+import random
+
+
+print("*****************************Wellcome To Rock Paper Sissor Game*****************************")
+
+#********************PLAYER CHOICE********************
+playerchoice=input("enter the any rps \n ROCK\nPAPER\nSSISOR\nenter the rps:")
+
+#*********************COMPUTER CHOICE*******************
+list=["rock","paper","sissor"]
+computerchoice=random.randrange(len(list))
+print("computer choice is :",list[computerchoice])
+
+#********************comperision the game*********************
+
+if playerchoice=="rock" and computerchoice=="sissor":
+    print("you win")
+elif playerchoice=="paper" and computerchoice=="rock":
+    print("you win the game")
+elif playerchoice=="sissor" and computerchoice=="paper":
+    print("you win")
+else:
+    print("computer win")
