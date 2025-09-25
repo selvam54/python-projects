f96b7053e4bc5665e0f836787e706046eb5d655a
selvam54 <s.selvam00000001@gmail.com>
2025-09-25 20:40:52 +0530

    add loop

--- rpsgame.py
+++ rpsgame.py
@@ -4,20 +4,20 @@ import random
 print("*****************************Wellcome To Rock Paper Sissor Game*****************************")
 
 #********************PLAYER CHOICE********************
-playerchoice=input("enter the any rps \n ROCK\nPAPER\nSSISOR\nenter the rps:")
-
-#*********************COMPUTER CHOICE*******************
-list=["rock","paper","sissor"]
-computerchoice=random.randrange(len(list))
-print("computer choice is :",list[computerchoice])
+while 1>-1:
+    playerchoice=input("enter the any rps \n ROCK\nPAPER\nSSISOR\nenter the rps:")
+    #*********************COMPUTER CHOICE*******************
+    list=["rock","paper","sissor"]
+    computerchoice=random.randrange(len(list))
+    print("computer choice is :",list[computerchoice])
 
 #********************comperision the game*********************
 
-if playerchoice=="rock" and computerchoice=="sissor":
-    print("you win")
-elif playerchoice=="paper" and computerchoice=="rock":
-    print("you win the game")
-elif playerchoice=="sissor" and computerchoice=="paper":
-    print("you win")
-else:
-    print("computer win")
+    if playerchoice=="rock" and computerchoice=="sissor":
+        print("you win")
+    elif playerchoice=="paper" and computerchoice=="rock":
+        print("you win the game")
+    elif playerchoice=="sissor" and computerchoice=="paper":
+        print("you win")
+    else:
+        print("computer win")
